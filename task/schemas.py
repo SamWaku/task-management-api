187514de3ed8e9ca462f676d5f5dcfac14e4b51e
SamWaku <SamuelWaku1st@gmail.com>
2024-09-17 12:44:48 +0200

    optionaltaskschema

--- task/schemas.py
+++ task/schemas.py
@@ -6,3 +6,8 @@ class Task(BaseModel):
     title: str
     duration: str
     completed: Optional[bool] = False
+
+class UpdateTask(BaseModel):
+    title: Optional[str] = None
+    duration: Optional[int] = None
+    completed: Optional[bool] = None
