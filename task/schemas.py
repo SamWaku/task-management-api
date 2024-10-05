9b49e0bb5cdce5dc36e51a171f74eff913011e61
SamWaku <SamuelWaku1st@gmail.com>
2024-10-05 04:23:46 +0200

    schema1

--- task/schemas.py
+++ task/schemas.py
@@ -21,8 +21,3 @@ class UpdateTask(BaseModel):
     title: Optional[str] = None
     duration: Optional[int] = None
     completed: Optional[bool] = None
-
-class User(BaseModel):
-    id: int
-    name:str
-    email:str
