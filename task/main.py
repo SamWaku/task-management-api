b8cd4225bd0ba8c482ad8015310768a5988fd59e
SamWaku <SamuelWaku1st@gmail.com>
2024-10-05 04:14:56 +0200

    response1

--- task/main.py
+++ task/main.py
@@ -96,3 +96,11 @@ def create_user(request: schemas.User, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get('/user/{id}', response_model=User)
+def get_user(id:int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with the id {id} is not found")
+    
+    return user
